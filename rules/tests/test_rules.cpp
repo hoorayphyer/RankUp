0e735fc38a4d9d865a10fed2f97104e3dbf43ba7
hooray <hrfudan@gmail.com>
2022-01-05 22:26:41 -0500

    tested `adjust_for_minor_lords` for two lordless cases

--- rules/tests/test_rules.cpp
+++ rules/tests/test_rules.cpp
@@ -225,44 +225,118 @@ SCENARIO("Overthrown Lordless Rules", "[rules]") {
   const Card lord(suit_lord, Rank::_8);
   auto rules = RulesLordlessOverthrown(lord);
 
-  WHEN("card comes before the lord card") {
-    Card card(Suit::S, Rank::_4);
-    auto value = rules.evaluate(card);
-    REQUIRE_FALSE(rules.is_lord(card));
-    CHECK((int)value.major() == static_cast<int>(Rank::_4));
-  }
+  SECTION("test `evaluate` and `is_lord`") {
+    WHEN("card comes before the lord card") {
+      Card card(Suit::S, Rank::_4);
+      auto value = rules.evaluate(card);
+      REQUIRE_FALSE(rules.is_lord(card));
+      CHECK((int)value.major() == static_cast<int>(Rank::_4));
+    }
 
-  WHEN("card comes after the lord card") {
-    Card card(Suit::S, Rank::_Q);
-    auto value = rules.evaluate(card);
-    REQUIRE_FALSE(rules.is_lord(card));
-    CHECK((int)value.major() == static_cast<int>(Rank::_Q) - 1);
-  }
+    WHEN("card comes after the lord card") {
+      Card card(Suit::S, Rank::_Q);
+      auto value = rules.evaluate(card);
+      REQUIRE_FALSE(rules.is_lord(card));
+      CHECK((int)value.major() == static_cast<int>(Rank::_Q) - 1);
+    }
 
-  WHEN("There are no major lords") {
-    for (auto suit : std::array<Suit, 4>{Suit::D, Suit::C, Suit::H, Suit::S}) {
-      Card card(suit, Rank::_8);
+    WHEN("There are no major lords") {
+      for (auto suit :
+           std::array<Suit, 4>{Suit::D, Suit::C, Suit::H, Suit::S}) {
+        Card card(suit, Rank::_8);
+        auto value = rules.evaluate(card);
+        REQUIRE(rules.is_lord(card));
+        CHECK(value.is_minor_lord());
+        CHECK((int)value.major() == 12);
+        CHECK((int)value.minor() == static_cast<int>(suit));
+      }
+    }
+
+    WHEN("card is a Joker") {
+      Card card(Suit::J, Rank::_w);
       auto value = rules.evaluate(card);
       REQUIRE(rules.is_lord(card));
-      CHECK(value.is_minor_lord());
-      CHECK((int)value.major() == 12);
-      CHECK((int)value.minor() == static_cast<int>(suit));
+      CHECK_FALSE(value.is_minor_lord());
+      CHECK((int)value.major() == 13);
+    }
+
+    WHEN("card is not a lord") {
+      CHECK_FALSE(rules.is_lord({Suit::D, Rank::_7}));
+      CHECK_FALSE(rules.is_lord({Suit::C, Rank::_10}));
+      CHECK_FALSE(rules.is_lord({Suit::H, Rank::_A}));
+      CHECK_FALSE(rules.is_lord({Suit::S, Rank::_5}));
     }
   }
 
-  WHEN("card is a Joker") {
-    Card card(Suit::J, Rank::_w);
-    auto value = rules.evaluate(card);
-    REQUIRE(rules.is_lord(card));
-    CHECK_FALSE(value.is_minor_lord());
-    CHECK((int)value.major() == 13);
-  }
+  SECTION("test `adjust_for_minor_lords`") {
+    WHEN("cards don't involve minor lords") {
+      std::vector<Value> values = {{9}, {9}, {11}, {13}};
+      const auto values_orig = values;
+      auto extra = rules.adjust_for_minor_lords(values);
+      REQUIRE(values == values_orig);
+      REQUIRE(extra == std::vector<Value>{});
+    }
 
-  WHEN("card is not a lord") {
-    CHECK_FALSE(rules.is_lord({Suit::D, Rank::_7}));
-    CHECK_FALSE(rules.is_lord({Suit::C, Rank::_10}));
-    CHECK_FALSE(rules.is_lord({Suit::H, Rank::_A}));
-    CHECK_FALSE(rules.is_lord({Suit::S, Rank::_5}));
+    WHEN("cards involve single minor lords") {
+      std::vector<Value> values = {{9},
+                                   {9},
+                                   {11},
+                                   {12, true, Suit::D},
+                                   {12, true, Suit::C},
+                                   {12, true, Suit::H},
+                                   {13}};
+      const auto values_orig = values;
+      auto extra = rules.adjust_for_minor_lords(values);
+      REQUIRE(values == values_orig);
+      REQUIRE(extra == std::vector<Value>{});
+    }
+
+    WHEN("cards involve pair minor lords but no pair of major lords") {
+      std::vector<Value> values = {{9},
+                                   {9},
+                                   {11},
+                                   {11},
+                                   {12, true, Suit::D},
+                                   {12, true, Suit::C},
+                                   {12, true, Suit::C},
+                                   {12, true, Suit::H},
+                                   {12, true, Suit::H},
+                                   {13}};
+      const auto values_orig = values;
+      auto extra = rules.adjust_for_minor_lords(values);
+      REQUIRE(values == values_orig);
+      REQUIRE(extra == std::vector<Value>{});
+    }
+
+    WHEN("cards involve pair minor lords and a pair of major lords") {
+      std::vector<Value> values = {{9},
+                                   {9},
+                                   {11},
+                                   {12, true, Suit::D},
+                                   {12, true, Suit::D},
+                                   {12, true, Suit::C},
+                                   {12, true, Suit::C},
+                                   {12, true, Suit::H},
+                                   {13},
+                                   {13}};
+      auto extra = rules.adjust_for_minor_lords(values);
+      THEN(
+          "singles in between must be shifted to before the last pair of minor "
+          "lords") {
+        const std::vector<Value> values_exp = {{9},
+                                               {9},
+                                               {11},
+                                               {12, true, Suit::D},
+                                               {12, true, Suit::D},
+                                               {12, true, Suit::H},
+                                               {12, true, Suit::C},
+                                               {12, true, Suit::C},
+                                               {13},
+                                               {13}};
+        REQUIRE(values == values_exp);
+        REQUIRE(extra == std::vector<Value>{});
+      }
+    }
   }
 }
 
@@ -271,22 +345,34 @@ SCENARIO("Regular Lordless Rules", "[rules]") {
   const Card lord(suit_lord, Rank::_w);
   auto rules = RulesLordlessRegular(lord);
 
-  WHEN("all cards except Jokers are folks") {
-    for (auto suit : std::array<Suit, 4>{Suit::D, Suit::C, Suit::H, Suit::S}) {
-      for (int8_t i = 0; i < 13; ++i) {
-        Card card(suit, static_cast<Rank>(i));
-        auto value = rules.evaluate(card);
-        REQUIRE_FALSE(rules.is_lord(card));
-        CHECK((int)value.major() == i);
+  SECTION("test `evaluate` and `is_lord`") {
+    WHEN("all cards except Jokers are folks") {
+      for (auto suit :
+           std::array<Suit, 4>{Suit::D, Suit::C, Suit::H, Suit::S}) {
+        for (int8_t i = 0; i < 13; ++i) {
+          Card card(suit, static_cast<Rank>(i));
+          auto value = rules.evaluate(card);
+          REQUIRE_FALSE(rules.is_lord(card));
+          CHECK((int)value.major() == i);
+        }
       }
+    }
+
+    WHEN("card is a Joker") {
+      Card card(Suit::J, Rank::_w);
+      auto value = rules.evaluate(card);
+      REQUIRE(rules.is_lord(card));
+      CHECK_FALSE(value.is_minor_lord());
+      CHECK((int)value.major() == 13);
     }
   }
 
-  WHEN("card is a Joker") {
-    Card card(Suit::J, Rank::_w);
-    auto value = rules.evaluate(card);
-    REQUIRE(rules.is_lord(card));
-    CHECK_FALSE(value.is_minor_lord());
-    CHECK((int)value.major() == 13);
+  SECTION("test `adjust_for_minor_lords`, which is no-op!") {
+    std::vector<Value> values = {{9},  {9},  {11}, {11}, {12},
+                                 {12}, {13}, {13}, {14}};
+    const auto values_orig = values;
+    auto extra = rules.adjust_for_minor_lords(values);
+    REQUIRE(values == values_orig);
+    REQUIRE(extra == std::vector<Value>{});
   }
 }
